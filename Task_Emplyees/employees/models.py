b292cdc96600c9b6e992dc5c995b5842d4f3e5bf
= <=>
2019-04-29 13:57:59 +0200

    add JobPosition model

--- Task_Emplyees/employees/models.py
+++ Task_Emplyees/employees/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class JobPosition (models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=50, blank=False)
+    date = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
