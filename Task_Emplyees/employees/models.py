13869fc5238e92676619a12674e3f98776ef285f
= <=>
2019-04-30 14:40:31 +0200

    change max_lenght Email field

--- Task_Emplyees/employees/models.py
+++ Task_Emplyees/employees/models.py
@@ -15,7 +15,7 @@ class Employee (models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     first_name = models.CharField(max_length=30, blank=False)
     last_name = models.CharField(max_length=30, blank=False)
-    email = models.EmailField(max_length=30, blank=False)
+    email = models.EmailField(blank=False)
     phone = models.BigIntegerField( validators=[RegexValidator(regex='^\d{9,15}$')],blank=False)
     date = models.DateTimeField(auto_now_add=True)
     last_update = models.DateTimeField(auto_now=True)
